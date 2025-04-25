53727cde9829eca340e0c42f2db6d00a8f539744
Álvaro López Romeralo <108227033+TMJgodofgame@users.noreply.github.com>
2025-04-25 13:25:39 +0200

    Update QuicKual install windows.bat
    añadidos los ficheros de comando de voz

--- QuicKual install windows.bat
+++ QuicKual install windows.bat
@@ -4,10 +4,16 @@ echo.
 
 set "ruta_descargas=%userprofile%\Downloads\QuicKual-main\QuicKual-main"
 set "ruta_destino=%appdata%\Microsoft\Templates"
+set "escritorio=%userprofile%\Desktop"
 
 echo.
 echo Moviendo o reemplazando normal.dotm en %ruta_destino%
 copy /y "%ruta_descargas%\normal.dotm" "%ruta_destino%\normal.dotm"
+
+echo.
+echo Moviendo scripts de QuicKual al escritorio
+move /y "%ruta_descargas%\quickual_con_voz_para_dictar(BETA).py" "%escritorio%"
+move /y "%ruta_descargas%\quickual_sin_voz_para_dictar(BETA).py" "%escritorio%"
 
 echo.
 echo ¡Operación completada con éxito!
